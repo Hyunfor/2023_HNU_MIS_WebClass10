6f23c41460d26bf6a131e1558aa24a3438019c72
Hyunfor <navel5148@gmail.com>
2023-05-15 10:27:09 +0900

    LoginServlet 생성, doGet부분 코딩

---
+++ src/com/mis/controller/LoginServlet.java
@@ -0,0 +1,60 @@
+package com.mis.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login.do")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO 로그인 화면 이동
+		String url = "member/login.jsp";
+		
+		/* 
+		 * 로그인 되어 있으면, main.jsp 이동
+		 * 로그인 되어 있지 않으면 로그인 페이지로 이동
+		*/
+		
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("loginUser") != null){
+			url = "main.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO 
+		
+	}
+
+}
