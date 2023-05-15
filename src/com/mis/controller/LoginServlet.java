9ef15398a7a5d42b4e1d5b41461ae3049c398d08
Hyunfor <navel5148@gmail.com>
2023-05-15 10:56:31 +0900

    MemberDAO, LoginServlet doPost 코딩

--- src/com/mis/controller/LoginServlet.java
+++ src/com/mis/controller/LoginServlet.java
@@ -10,6 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.mis.dao.MemberDAO;
+import com.mis.dto.MemberVO;
+import com.sun.media.jfxmedia.MediaPlayer;
+
 /**
  * Servlet implementation class LoginServlet
  */
@@ -54,8 +58,33 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO 회원 인증 기능 -> 회원 인증하면 main.jsp 이동, 실패하면 login.jsp로 이동
+		String url = "member/login.jsp";
 		
+		String userid = request.getParameter("userid");
+		String pwd = request.getParameter("pwd");
 		
+		MemberDAO mDao = MemberDAO.getInstance();
+		int result = mDao.userCheck(userid, pwd);
+		
+		if(result == 1){
+			// User 정보 가져와 Session 담기
+			
+			MemberVO mVo = mDao.getMember(userid);
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mVo);
+			request.setAttribute("message", "회원가입에 성공했습니다.");
+			
+			url = "main.jsp";
+			
+		} else if(result == 0) {
+			request.setAttribute("message", "비밀번호가 일치하지 않습니다.");
+		} else if(result == -1) {
+			request.setAttribute("message", "등록된 사용자 정보가 없습니다.");
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 		
 	}
 
--- src/com/mis/dao/MemberDAO.java
+++ src/com/mis/dao/MemberDAO.java
@@ -8,72 +8,106 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import com.mis.dto.MemberVO;
+
 public class MemberDAO {
 
-	private MemberDAO(){ // 생성자
-		
+	private MemberDAO() { // 생성자
+
 	}
-	
+
 	private static MemberDAO instance = new MemberDAO();
-	
-	public static MemberDAO getInstance(){
-		
+
+	public static MemberDAO getInstance() {
+
 		return instance;
 	}
-	
+
 	public Connection getConnection() throws Exception {
-		
+
 		Context initContext = new InitialContext();
 		Context envContext = (Context) initContext.lookup("java:/comp/env");
 		DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
 		Connection conn = ds.getConnection();
-		
+
 		return conn;
-		
+
 	}
-	
-	
+
 	// 사용자 인증시 사용하는 메소드 => 로그인 기능
-	public int userCheck(String userid, String pwd){
-		
+	public int userCheck(String userid, String pwd) {
+
 		int result = -1; // -1은 회원정보가 없으면 -1로 리턴
 		String sql = "SELECT PWD FROM MEMBER WHERE USERID=?";
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
-			
+
 			conn = getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, userid);
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()){
+
+			if (rs.next()) {
 				// 일치하는 userid 존재
-				
-				if(rs.getString("pwd") != null && rs.getString("pwd").equals(pwd)){
+
+				if (rs.getString("pwd") != null && rs.getString("pwd").equals(pwd)) {
 					// 비밀번호 일치
 					result = 1;
 				} else {
 					// 비밀번호 불일치. 0
 					result = 0;
-					
+
 				}
-				
+
 			} else {
 				// 일치하는 userid 없음
 				result = -1;
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
-		
+
 	}
-	
-	
+
+	// 사용자 정보 가져오는 기능
+	public MemberVO getMember(String userid) {
+		MemberVO mVo = null;
+		String sql = "SELECT * FROM MEMBER WHERE USERID=?";
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				mVo = new MemberVO();
+				mVo.setName(rs.getString("name"));
+				mVo.setUserid(rs.getString("userid"));
+				mVo.setPwd(rs.getString("pwd"));
+				mVo.setEmail(rs.getString("email"));
+				mVo.setPhone(rs.getString("phone"));
+				mVo.setAdmin(rs.getInt("admin"));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return mVo;
+
+	}
+
 }
